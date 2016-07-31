f8b0a835d0fe146c48ebdfd4e85bb0bbc09d23e4
Chiun Hau You <chiunhau9@gmail.com>
2016-07-31 14:18:26 +0800

    seperate collection and project commands

--- p5-new.js
+++ p5-new.js
@@ -4,43 +4,44 @@ var program = require('commander');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 var path = require('path');
+
+// load templates
 var p5js = loadTemplate('p5.js');
-var p5dminjs = loadTemplate('p5.min.js');
+var p5minjs = loadTemplate('p5.min.js');
 var p5domjs = loadTemplate('p5.dom.js');
 var p5soundjs = loadTemplate('p5.sound.js');
 var sketchjs = loadTemplate('sketch.js');
 var indexhtml = loadTemplate('index.html');
 
 program
-	.version('0.0.1')
-	.option('-c, --collection <cname>', 'initialize a p5 sketches collection')
-	.parse(process.argv);
-
-if(program.collection) {
-	mkdir(program.collection, function() {
-		mkdir(program.collection + '/libraries', function() {
-			write(program.collection + '/libraries/p5.js', p5js);
-			write(program.collection + '/libraries/p5.min.js', p5minjs);
-			write(program.collection + '/libraries/p5.sound.js', p5soundjs);
-			write(program.collection + '/libraries/p5.dom.js', p5domjs);
-		});
+	.command('new <collection>')
+	.alias('n')
+	.description('Create new p5 collection')
+	.action(function(req, opt) {
+		mkdir(req, function() {
+			mkdir(req + '/libraries', function() {
+				write(req + '/libraries/p5.js', p5js);
+				write(req + '/libraries/p5.min.js', p5minjs);
+				write(req + '/libraries/p5.sound.js', p5soundjs);
+				write(req + '/libraries/p5.dom.js', p5domjs);
+			});
+		})
 	})
-}
 
 program
-	.command('new <project name>')
-	.description('generate a new p5 sketch')
+	.command('generate <project>')
+	.alias('g')
+	.description('Generate a p5 project')
 	.action(function(req, opt) {
-		var projectPath = req;
-		mkdir(projectPath, function() {
-			write(projectPath + '/sketch.js', sketchjs);
-			write(projectPath + '/index.html', indexhtml);	
+		mkdir(req, function() {
+			write(req + '/sketch.js', sketchjs);
+			write(req + '/index.html', indexhtml);	
 		});
 	});
 
-
 program.parse(process.argv);
 
+// code by https://github.com/expressjs/generator
 
 function loadTemplate(name) {
   return fs.readFileSync(path.join(__dirname, 'static', name), 'utf-8');
